859ffc0900715c57f01c3ed00029ea32d9b4fa1b
sudharsan R <41150869+sudharsanrj@users.noreply.github.com>
2021-03-02 21:45:21 +0530

    Add files via upload

---
+++ MOQ/PlayersManagerLib1/PlayersManagerLib1/PlayerManager.Tests/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using PlayersManagerLib;
+using NUnit.Compatibility;
+
+
+namespace PlayerManager.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        Mock<IPlayerMapper> mock;
+        [OneTimeSetUp]
+        public void init()
+        {
+            mock = new Mock<IPlayerMapper>();
+        }
+        [Test]
+        [TestCase("")]
+        public void EmptyName(string name)
+        {
+            Player player;
+            Assert.That(() => player = Player.RegisterNewPlayer(name, mock.Object), Throws.TypeOf<ArgumentException>());
+            
+        }
+        [Test]
+        [TestCase("Sudharsan")]
+        public void AlreadyInDb(string name)
+        {
+            mock.Setup(x => x.IsPlayerNameExistsInDb(name)).Returns(true);
+            Player player; 
+            Assert.That(() => player = Player.RegisterNewPlayer(name, mock.Object), Throws.TypeOf<ArgumentException>());
+        }
+    
+    [Test]
+    [TestCase("Sudharsan","India")]
+
+    public void NotInDb(string name,string country)
+        {
+            mock.Setup(x => x.IsPlayerNameExistsInDb(name)).Returns(false);
+            Player player = Player.RegisterNewPlayer(name, mock.Object);
+            Assert.AreEqual(name, player.Name);
+            Assert.AreEqual(20, player.Age);
+
+            Assert.AreEqual(country, player.Country);
+            Assert.AreEqual(20, player.NoOfMatches);
+
+        }
+    }
+}
