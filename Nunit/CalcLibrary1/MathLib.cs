38807f87d676716e082e9ab6fe08bd409dd64191
sudharsan R <41150869+sudharsanrj@users.noreply.github.com>
2021-03-02 10:43:06 +0530

    Add files via upload

---
+++ Nunit/CalcLibrary1/MathLib.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcLibrary1
+{
+    public class MathLib
+    {
+        public interface IMathLibrary
+        {
+            double Addition(double a, double b);
+            double Subtraction(double a, double b);
+            double Multiplication(double a, double b);
+            double Division(double a, double b);
+        }
+        public class SimpleCalculator : IMathLibrary
+        {
+            double result = 0;
+            public double Addition(double a, double b)
+            {
+                result = a + b;
+                return result;
+            }
+            public double Multiplication(double a, double b)
+            {
+                result = a * b;
+                return result;
+            }
+            public double Subtraction(double a, double b)
+            {
+                result = a - b;
+                return result;
+            }
+            public double Division(double a, double b)
+            {
+               
+                result = a / b;
+                return result;
+            }
+
+            public void AllClear()
+            {
+                result = 0;
+            }
+
+            public double GetResult
+            {
+                get { return result; }
+            }
+
+        }
+    }
+}
