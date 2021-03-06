4f16f5b026d83c2bb00021ef911c5b1eb7547750
sudharsan R <41150869+sudharsanrj@users.noreply.github.com>
2021-03-06 10:53:07 +0530

    Add files via upload

---
+++ C#-ADDTIONAL/Async&WindowsForms/WindowsForm/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int CountCharacters() 
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader("C:\\Users\\sudha\\source\\repos\\WindowsFormsApp1\\ddad.txt"))
+            {
+
+                string content = reader.ReadToEnd();
+                count = content.Length;
+                Thread.Sleep(5000);
+            }
+            return count;
+        }
+
+        private async  void btnProcessFile(object sender, EventArgs e)
+        {
+            Task<int> task = new Task<int>(CountCharacters);
+            task.Start();
+
+            LblCount.Text = "Processing File. Please Wait...";
+            int count = await task;
+            LblCount.Text = count.ToString() + " characters in file.";
+        }
+    }
+}
