194e6b906e95552d30ae5da151857fcb3b1981ab
sudharsan R <41150869+sudharsanrj@users.noreply.github.com>
2021-03-07 01:13:51 +0530

    Add files via upload

---
+++ Asp.NetCore/Asp.netOrganisations/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Asp.netOrganisations.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.netOrganisations.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public ActionResult Organisations()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
