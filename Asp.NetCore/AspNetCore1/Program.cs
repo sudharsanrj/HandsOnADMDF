71de41b724acda1553117072f3c691a69ae34001
sudharsan R <41150869+sudharsanrj@users.noreply.github.com>
2021-03-07 00:02:45 +0530

    Add files via upload

---
+++ Asp.NetCore/AspNetCore1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AspNetCore1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
