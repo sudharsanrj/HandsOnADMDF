3abf18273830058c52b5b9d1d7f5310715536242
sudharsan R <41150869+sudharsanrj@users.noreply.github.com>
2021-03-07 17:40:29 +0530

    Add files via upload

---
+++ Asp.NetCore/Scaffolding/Data/AccountConContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Account_Scaffold.Models;
+
+namespace Account_Scaffold.Data
+{
+    public class AccountConContext : DbContext
+    {
+        public AccountConContext (DbContextOptions<AccountConContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Account_Scaffold.Models.Account> Account { get; set; }
+    }
+}
---
+++ Asp.NetCore/Scaffolding/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Account_Scaffold.Models
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public string AccountName { get; set; }
+    }
+}
