80957e9da73b3870cef674fceb958ed44a9e4233
sudharsan R <41150869+sudharsanrj@users.noreply.github.com>
2021-03-21 23:46:13 +0530

    Add files via upload

---
+++ Design-Pattern/Singleton/Singleton/DBConn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class DBConn
+    {
+        private static DBConn instance;
+        private DBConn()
+        {
+            Console.WriteLine("Singleton Instance");
+        }
+        public static DBConn getInstance()
+        {
+            if (DBConn.instance == null)
+            {
+                DBConn.instance = new DBConn();
+                
+            }
+            return DBConn.instance;
+        }
+        public void PrintDetails(string name)
+        {
+            Console.WriteLine("Hi " +name);
+        }
+    }
+}
+
---
+++ Design-Pattern/Singleton/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConn instance = DBConn.getInstance();
+            DBConn instance1 = DBConn.getInstance();
+            instance.PrintDetails("Bruce");
+            instance1.PrintDetails("Barry");
+            Console.WriteLine(instance.GetHashCode());
+            Console.WriteLine(instance1.GetHashCode());     
+            Console.ReadLine();
+        }
+    }
+}
